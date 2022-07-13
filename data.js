2df9b81194b95e63a3becaff310ef10932d62df4
Nader Boutros <nader.boutros@pass-tech.fr>
2022-07-13 06:39:35 +0200

    Tooling and organizing

---
+++ data.js
@@ -0,0 +1,169 @@
+const spaces = [
+    {
+        "title" : "Test files",
+        "description" : "Experimental files",
+        "projects": [
+            {
+                "title" : "IFC.JS test",
+                "description" : "Experimental files",
+                "files":[
+                    {
+                        "file" : './assets/ifc/test.ifc',
+                        "type" : "IFC",
+                        "description" : "Example file",
+                        "source" : "Converted from Revit",
+                        "rights" : "CC"
+                    },
+                ]
+            },
+            {
+                "title" : "Three.js tests",
+                "description" : "Experimental files",
+                "files":[
+                    {
+                        "file" : './assets/gltf/nefertiti/scene.gltf',
+                        "type" : "GLTF",
+                        "description" : "Example file",
+                        "source" : "Sketchfab",
+                        "rights" : "CC"
+                    },
+                    {
+                        "file" : './assets/gltf/nhm_london/scene.gltf',
+                        "type" : "GLTF",
+                        "description" : "Example file",
+                        "source" : "Sketchfab",
+                        "rights" : "CC"
+                    },
+                    {
+                        "file" : './assets/gltf/sailing_ship/scene.gltf',
+                        "type" : "GLTF",
+                        "description" : "Example file",
+                        "source" : "Sketchfab",
+                        "rights" : "CC"
+                    },
+                    {
+                        "file" : './assets/gltf/the_great_drawing_room/scene.gltf',
+                        "type" : "GLTF",
+                        "description" : "Example file",
+                        "source" : "Sketchfab",
+                        "rights" : "CC"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "title" : "ENSAPVS : Revit Initiation",
+        "description" : "Best students works",
+        "projects" : [
+            {
+                "title" : "Final project",
+                "description" : "Eglantine's work",
+                "files" : [
+                    {
+                        "file" : './school/L1/2022/eglantine/rendu.glb',
+                        "type" : 'GLTF',
+                        "description" : "Eglantine",
+                        "source" : "Revit file conversion",
+                        "Rights" : "Credits to Eglantine"
+                    },
+                    {
+                        "file" : './school/L1/2022/eglantine/rendu.pdf',
+                        "type" : 'PDF',
+                        "description" : "Eglantine",
+                        "source" : "Project presentation",
+                        "Rights" : "Credits to Eglantine"
+                    }
+                ]
+            },
+            {
+                "title" : "Final project",
+                "description" : "Nils' work",
+                "files" : [
+                    {
+                        "file" : './school/L1/2022/nils/rendu.glb',
+                        "type" : 'GLTF',
+                        "description" : "Nils",
+                        "source" : "Revit file conversion",
+                        "Rights" : "Credits to Nils"
+                    },
+                    {
+                        "file" : './school/L1/2022/nils/rendu.pdf',
+                        "type" : 'PDF',
+                        "description" : "Nils",
+                        "source" : "Project presentation",
+                        "Rights" : "Credits to Nils"
+                    }
+                ]
+            },            {
+                "title" : "Final project",
+                "description" : "Marie's work",
+                "files" : [
+                    {
+                        "file" : './school/L1/2022/marie/rendu.glb',
+                        "type" : 'GLTF',
+                        "description" : "Marie",
+                        "source" : "Revit file conversion",
+                        "Rights" : "Credits to Marie"
+                    },
+                    {
+                        "file" : './school/L1/2022/marie/rendu.pdf',
+                        "type" : 'PDF',
+                        "description" : "Marie",
+                        "source" : "Project presentation",
+                        "Rights" : "Credits to Marie"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        "title" : "ENSAPVS : Computational design lab (CDL)",
+        "description" : "Best students works",
+        "projects" : []
+    },
+    {
+        "title" : "ENSAPVS : Construction & ambiance",
+        "description" : "Best students works",
+        "projects" : []
+    },
+    {
+        "title" : "ENSAPVS : Seminar MN2BIM",
+        "description" : "Best students works",
+        "projects" : []
+    },
+    {
+        "title" : "ENSAPVS : Digital tools",
+        "description" : "Best students works",
+        "projects" : []
+    },
+    {
+        "title" : "PASS-TECH : Training exercices",
+        "description" : "Training exercices used in training courses",
+        "projects" : []
+    },
+    {
+        "title" : "EPFL : BIM courses",
+        "description" : "Best students works",
+        "projects" : [],
+        "members" : []
+    },
+    {
+        "title" : "SolidariBIM : Training exercices",
+        "description" : "Training exercices used in training courses",
+        "projects" : [],
+        "members" : []
+    },
+    {
+        "title" : "HDCP : Heritage in Danger Collaborative Platform",
+        "description" : "Prototype for HDCP project",
+        "projects" : [
+            {
+                "title" : "Ukraine mission - June 2022",
+                "description" : "",
+                "files" : []
+            }
+        ],
+        "members" : []
+    },
+]
--- index.js
+++ index.js
@@ -1,8 +1,148 @@
-import { Mesh } from 'three';
+import * as THREE from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { Vector3 } from 'three';
 
-console.log(Mesh);
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new THREE.Color(0xffffff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+const scene = viewer.context.getScene();
+let xposition = 0;
+let mouseHelper;
+let cones=[];
+const params = {
+    minScale: 10,
+    maxScale: 20,
+    rotate: true,
+    clear: function () {
+        removeCones();
+    }
+};
 
-const mesh = new Mesh();
+//Initial cone
+const geometry1 = new THREE.ConeGeometry( 0.1, 0.3, 5);
+const material1 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
 
-console.log(mesh);
-console.log(mesh.geometry);
+window.addEventListener( 'load', init );
+
+function init() {
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setFromPoints( [ new THREE.Vector3(), new THREE.Vector3() ] );
+
+    line = new THREE.Line( geometry, new THREE.LineBasicMaterial() );
+    scene.add( line );
+
+    mouseHelper = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 3 ), new THREE.MeshNormalMaterial() );
+    mouseHelper.visible = false;
+    scene.add( mouseHelper );
+
+
+    //show files
+    for (let f of files) {
+        if (f.type === "IFC") loadIfc(f.file);
+        if (f.type === "GLTF") loadGltf(f.file);
+    } 
+
+    const gui = new GUI();
+
+    gui.add( params, 'minScale', 1, 30 );
+    gui.add( params, 'maxScale', 1, 30 );
+    gui.add( params, 'rotate' );
+    gui.add( params, 'clear' );
+    gui.open();
+
+
+}
+
+async function loadIfc(url) {
+    await viewer.IFC.setWasmPath("./assets/wasm/");
+    const model = await viewer.IFC.loadIfcUrl(url);
+    // viewer.shadowDropper.renderShadow(model.modelID);   
+    model.position.x = xposition;
+    xposition += 10;
+}
+
+async function loadGltf(url) {
+    const model = await viewer.GLTF.load(url);
+    model.position.z = xposition;
+    xposition += 20;
+}
+
+function checkIntersection(){
+    let clicPosition = viewer.context.castRayIfc();
+    console.log('Intersection : ', clicPosition);
+    const p = clicPosition?.point;
+    let n = new Vector3();
+    n = clicPosition?.face?.normal.clone();
+    const cone = new THREE.Mesh( geometry1, material1 );
+    scene.add( cone );
+    cone.position.copy(p);
+    let quaternion = new THREE.Quaternion();
+    let up = new THREE.Vector3(0,-1,0);
+    quaternion.setFromUnitVectors(up, n);
+    cone.setRotationFromQuaternion(quaternion);
+    cones.push(cone);
+}
+
+const files = [
+    {
+        "file" : './assets/ifc/test.ifc',
+        "type" : "IFC",
+        "description" : "Example file",
+        "source" : "IFC.JS"
+    },
+    {
+        "file" : './assets/gltf/nefertiti/scene.gltf',
+        "type" : "GLTF",
+        "description" : "Example file",
+        "source" : "Sketchfab"
+    },
+    {
+        "file" : './assets/gltf/nhm_london/scene.gltf',
+        "type" : "GLTF",
+        "description" : "Example file",
+        "source" : "Sketchfab"
+    },
+    {
+        "file" : './assets/gltf/sailing_ship/scene.gltf',
+        "type" : "GLTF",
+        "description" : "Example file",
+        "source" : "Sketchfab"
+    },
+    {
+        "file" : './assets/gltf/the_great_drawing_room/scene.gltf',
+        "type" : "GLTF",
+        "description" : "Example file",
+        "source" : "Sketchfab"
+    }
+];
+
+//Catch events
+let moved = false;
+
+addEventListener( 'change', () => {moved = true;});
+addEventListener( 'pointerdown', () => {moved = false;});
+addEventListener('dblclick', () => { 
+    if ( moved === false ) {
+        checkIntersection(); 
+    }
+});
+addEventListener( 'pointermove', onPointerMove );
+
+function onPointerMove( event ) {
+    if ( event.isPrimary ) {
+        
+    }
+}
+
+function removeCones() {
+    cones.forEach( function ( cone ) {
+
+        scene.remove( cone );
+
+    } );
+
+    cones.length = 0;
+}
